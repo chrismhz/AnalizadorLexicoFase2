436f559712a5d70c520da272806718ebb54e9dd5
chrismhz <martinez.christian.isic@gmail.com>
2023-12-25 22:55:08 -0600

    Se agrega el Proyecto

---
+++ src/codigo/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package codigo;
+
+import java.io.File;
+
+/**
+ *
+ * @author Christian
+ */
+public class Principal {
+    public static void main(String[] args) {
+        String ruta = "C:/Users/Christian/Documents/NetBeansProjects/AnalizadorLexico/src/codigo/Lexer.flex";
+        generarLexer(ruta);  
+    }
+    public static void generarLexer(String ruta){
+        File archivo = new File(ruta);
+        JFlex.Main.generate(archivo);
+    }
+}
+
